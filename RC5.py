a5d87b86e40e641596a1523873efc0be19ac77c6
tbb <alisha.one@mail.ru>
2015-09-13 01:04:57 +0300

    Last changes

--- RC5.py
+++ RC5.py
@@ -1,5 +1,4 @@
 # -*- coding: utf-8 -*-
-import platform
 
 
 class RC5:
@@ -16,11 +15,11 @@ class RC5:
     def __convert(self, val):
         return list(bin(abs(val))[2:].zfill(self.W))
 
-    def __shift(self, val, n):
+    def __lshift(self, val, n):
         val, n = self.__convert(val), n % self.W
         return int(''.join(val[n:] + val[:n]), 2)
 
-    def __unshift(self, val, n):
+    def __rshift(self, val, n):
         val, n = self.__convert(val), n % self.W
         return int(''.join(val[-n:] + val[:-n]), 2)
 
@@ -50,8 +49,8 @@ class RC5:
         m = max(self.c, 2 * self.R + 1)
         i, j, A, B = 0, 0, 0, 0
         for k in range(3 * m):
-            A = self.S[i] = self.__shift((self.S[i] + A + B), 3)
-            B = self.L[j] = self.__shift((self.L[j] + A + B), A + B)
+            A = self.S[i] = self.__lshift((self.S[i] + A + B), 3)
+            B = self.L[j] = self.__lshift((self.L[j] + A + B), A + B)
             i = (i + 1) % (2 * self.R + 1)
             j = (j + 1) % self.c
 
@@ -67,9 +66,9 @@ class RC5:
                 A = (A + self.S[0]) % 2 ** self.W
                 B = (B + self.S[1]) % 2 ** self.W
                 for i in range(1, self.R):
-                    A = (self.__shift((A ^ B), B)
+                    A = (self.__lshift((A ^ B), B)
                          + self.S[2 * i]) % 2 ** self.W
-                    B = (self.__shift((A ^ B), A)
+                    B = (self.__lshift((A ^ B), A)
                          + self.S[2 * i + 1]) % 2 ** self.W
 
                 out.write(A.to_bytes(self.W // 8, byteorder='big') +
@@ -84,9 +83,9 @@ class RC5:
                 A = int.from_bytes(text[:self.W // 8], byteorder='big')
                 B = int.from_bytes(text[self.W // 8:], byteorder='big')
                 for i in range(self.R - 1, 0, -1):
-                    B = self.__unshift(
+                    B = self.__rshift(
                         ((B - self.S[2 * i + 1]) % 2 ** self.W), A) ^ A
-                    A = self.__unshift(
+                    A = self.__rshift(
                         ((A - self.S[2 * i]) % 2 ** self.W), B) ^ B
                 B = (B - self.S[1]) % 2 ** self.W
                 A = (A - self.S[0]) % 2 ** self.W
