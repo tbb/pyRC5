939f613c41c1d9316b1506b56ee785730808766d
tbb <alisha.one@mail.ru>
2015-09-24 15:38:29 +0300

    Fix lshift, rshift. Add tests

--- RC5.py
+++ RC5.py
@@ -1,94 +1,133 @@
-# -*- coding: utf-8 -*-
-
-
 class RC5:
 
-    def __init__(self, W, R, key):
-        self.W = W
+    def __init__(self, w, R, key):
+        self.w = w
         self.R = R
-        self.b = len(key)
         self.key = key
+        self.T = 2 * (R + 1)
+        self.w4 = w // 4
+        self.w8 = w // 8
+        self.mod = 2 ** self.w
+        self.mask = self.mod - 1
+        self.b = len(key)
         self.__keyAlign()
         self.__keyExtend()
         self.__shuffle()
 
-    def __convert(self, val):
-        return list(bin(abs(val))[2:].zfill(self.W))
-
     def __lshift(self, val, n):
-        val, n = self.__convert(val), n % self.W
-        return int(''.join(val[n:] + val[:n]), 2)
+        n %= self.w
+        return ((val << n) & self.mask) | ((val & self.mask) >> (self.w - n))
 
     def __rshift(self, val, n):
-        val, n = self.__convert(val), n % self.W
-        return int(''.join(val[-n:] + val[:-n]), 2)
+        n %= self.w
+        return ((val & self.mask) >> n) | (val << (self.w - n) & self.mask)
 
     def __const(self):
-        if self.W == 16:
+        if self.w == 16:
             return (0xB7E1, 0x9E37)
-        elif self.W == 32:
+        elif self.w == 32:
             return (0xB7E15163, 0x9E3779B9)
-        elif self.W == 64:
+        elif self.w == 64:
             return (0xB7E151628AED2A6B, 0x9E3779B97F4A7C15)
 
     def __keyAlign(self):
-        while len(self.key) % (self.W // 8):
-            self.key += b'\x00'
-        self.c = len(self.key) // (self.W // 8)
-        L, key = [], bin(int.from_bytes(self.key, byteorder='little'))[2:]
-        for i in range(self.c):
-            L.append(int(key[:self.W], 2))
-            key = key[self.W:]
+        if self.b == 0:
+            self.c = 1
+        elif self.b % self.w8:
+            self.key += b'\x00' * (self.w8 - self.b % self.w8)
+            self.b = len(self.key)
+            self.c = self.b // self.w8
+        else:
+            self.c = self.b // self.w8
+        L = [0] * self.c
+        for i in range(self.b - 1, -1, -1):
+            L[i // self.w8] = (L[i // self.w8] << 8) + self.key[i]
         self.L = L
 
     def __keyExtend(self):
         P, Q = self.__const()
-        self.S = [(P + i * Q) % 2 ** self.W for i in range(2 * self.R + 1)]
+        self.S = [(P + i * Q) % self.mod for i in range(self.T)]
 
     def __shuffle(self):
-        m = max(self.c, 2 * self.R + 1)
         i, j, A, B = 0, 0, 0, 0
-        for k in range(3 * m):
+        for k in range(3 * max(self.c, self.T)):
             A = self.S[i] = self.__lshift((self.S[i] + A + B), 3)
             B = self.L[j] = self.__lshift((self.L[j] + A + B), A + B)
-            i = (i + 1) % (2 * self.R + 1)
+            i = (i + 1) % self.T
             j = (j + 1) % self.c
 
-    def encrypt(self, inpFileName, outFileName):
+    def encryptBlock(self, data):
+        A = int.from_bytes(data[:self.w8], byteorder='little')
+        B = int.from_bytes(data[self.w8:], byteorder='little')
+        A = (A + self.S[0]) % self.mod
+        B = (B + self.S[1]) % self.mod
+        for i in range(1, self.R + 1):
+            A = (self.__lshift((A ^ B), B) + self.S[2 * i]) % self.mod
+            B = (self.__lshift((A ^ B), A) + self.S[2 * i + 1]) % self.mod
+        return (A.to_bytes(self.w8, byteorder='little')
+                + B.to_bytes(self.w8, byteorder='little'))
+
+    def decryptBlock(self, data):
+        A = int.from_bytes(data[:self.w8], byteorder='little')
+        B = int.from_bytes(data[self.w8:], byteorder='little')
+        for i in range(self.R, 0, -1):
+            B = self.__rshift(B - self.S[2 * i + 1], A) ^ A
+            A = self.__rshift(A - self.S[2 * i], B) ^ B
+        B = (B - self.S[1]) % self.mod
+        A = (A - self.S[0]) % self.mod
+        return (A.to_bytes(self.w8, byteorder='little')
+                + B.to_bytes(self.w8, byteorder='little'))
+
+    def encryptFile(self, inpFileName, outFileName):
         with open(inpFileName, 'rb') as inp, open(outFileName, 'wb') as out:
-            while True:
-                text = inp.read(self.W // 4)
+            run = True
+            while run:
+                text = inp.read(self.w4)
                 if not text:
                     break
-                text = text.ljust(self.W // 4, b'\x00')
-                A = int.from_bytes(text[:self.W // 8], byteorder='little')
-                B = int.from_bytes(text[self.W // 8:], byteorder='little')
-                A = (A + self.S[0]) % 2 ** self.W
-                B = (B + self.S[1]) % 2 ** self.W
-                for i in range(1, self.R):
-                    A = (self.__lshift((A ^ B), B)
-                         + self.S[2 * i]) % 2 ** self.W
-                    B = (self.__lshift((A ^ B), A)
-                         + self.S[2 * i + 1]) % 2 ** self.W
+                if len(text) != self.w4:
+                    text = text.ljust(self.w4, b'\x00')
+                    run = False
+                text = self.encryptBlock(text)
+                out.write(text)
 
-                out.write(A.to_bytes(self.W // 8, byteorder='little') +
-                          B.to_bytes(self.W // 8, byteorder='little'))
-
-    def decrypt(self, inpFileName, outFileName):
+    def decryptFile(self, inpFileName, outFileName):
         with open(inpFileName, 'rb') as inp, open(outFileName, 'wb') as out:
-            while True:
-                text = inp.read(self.W // 4)
+            run = True
+            while run:
+                text = inp.read(self.w4)
                 if not text:
                     break
-                A = int.from_bytes(text[:self.W // 8], byteorder='little')
-                B = int.from_bytes(text[self.W // 8:], byteorder='little')
-                for i in range(self.R - 1, 0, -1):
-                    B = self.__rshift(
-                        ((B - self.S[2 * i + 1]) % 2 ** self.W), A) ^ A
-                    A = self.__rshift(
-                        ((A - self.S[2 * i]) % 2 ** self.W), B) ^ B
-                B = (B - self.S[1]) % 2 ** self.W
-                A = (A - self.S[0]) % 2 ** self.W
-                res = (A.to_bytes(self.W // 8, byteorder='little')
-                       + B.to_bytes(self.W // 8, byteorder='little'))
-                out.write(res.rstrip(b'\x00'))
+                if len(text) != self.w4:
+                    run = False
+                text = self.decryptBlock(text)
+                if not run:
+                    text = text.rstrip(b'\x00')
+                out.write(text)
+
+    def encryptBytes(self, data):
+        res, run = b'', True
+        while run:
+            temp = data[:self.w4]
+            if len(temp) != self.w4:
+                data = data.ljust(self.w4, b'\x00')
+                run = False
+            res += self.encryptBlock(temp)
+            data = data[self.w4:]
+            if not data:
+                break
+        return res
+
+    def decryptBytes(self, data):
+        res, run = b'', True
+        while run:
+            temp = data[:self.w4]
+            if len(temp) != self.w4:
+                run = False
+            res += self.decryptBlock(temp)
+            if not run:
+                res = res.rstrip(b'\x00')
+            data = data[self.w4:]
+            if not data:
+                break
+        return res
--- test.py
+++ test.py
@@ -3,39 +3,12 @@ import unittest
 import os
 import tempfile
 
-INIT_DATA = """
-        The Zen of Python, by Tim Peters
 
-        Beautiful is better than ugly.
-        Explicit is better than implicit.
-        Simple is better than complex.
-        Complex is better than complicated.
-        Flat is better than nested.
-        Sparse is better than dense.
-        Readability counts.
-        Special cases aren't special enough to break the rules.
-        Although practicality beats purity.
-        Errors should never pass silently.
-        Unless explicitly silenced.
-        In the face of ambiguity, refuse the temptation to guess.
-        There should be one-- and preferably only one --obvious way to do it.
-        Although that way may not be obvious at first unless you're Dutch.
-        Now is better than never.
-        Although never is often better than *right* now.
-        If the implementation is hard to explain, it's a bad idea.
-        If the implementation is easy to explain, it may be a good idea.
-        Namespaces are one honking great idea -- let's do more of those!
-        """
-TEST_KEY = b"MontyPython"
-
-
-class RC5TestCase(unittest.TestCase):
+class RC5FileTests(unittest.TestCase):
 
     def setUp(self):
         self.input_fd, self.input_filename = tempfile.mkstemp(dir=os.curdir)
         self.out_fd, self.out_filename = tempfile.mkstemp(dir=os.curdir)
-        with open(self.input_filename, 'w') as data_file:
-            data_file.write(INIT_DATA)
 
     def tearDown(self):
         os.close(self.input_fd)
@@ -43,48 +16,111 @@ class RC5TestCase(unittest.TestCase):
         os.close(self.out_fd)
         os.unlink(self.out_filename)
 
-    def test_RC5_six_rounds_crypt(self):
-        testRC5 = RC5.RC5(64, 6, TEST_KEY)
-        testRC5.encrypt(self.input_filename, self.out_filename)
-        testRC5.decrypt(self.out_filename, self.input_filename)
-        with open(self.input_filename, 'r') as f:
-            assert INIT_DATA == f.read()
+    def test1(self):
+        testRC5 = RC5.RC5(32, 12, b'\0' * 16)
+        plainText = b'\0' * 8
+        with open(self.input_filename, 'wb') as data_file:
+            data_file.write(plainText)
+        testRC5.encryptFile(self.input_filename, self.out_filename)
+        cipherText = b'\x21\xA5\xDB\xEE\x15\x4B\x8F\x6D'
+        with open(self.out_filename, 'rb') as data_file:
+            assert cipherText == data_file.read()
+        testRC5.decryptFile(self.out_filename, self.input_filename)
+        with open(self.input_filename, 'rb') as data_file:
+            assert plainText == data_file.read()
 
-    def test_RC5_twelve_rounds_crypt(self):
-        testRC5 = RC5.RC5(64, 12, TEST_KEY)
-        testRC5.encrypt(self.input_filename, self.out_filename)
-        testRC5.decrypt(self.out_filename, self.input_filename)
-        with open(self.input_filename, 'r') as f:
-            assert INIT_DATA == f.read()
+    def test2(self):
+        key = b'\x91\x5F\x46\x19\xBE\x41\xB2\x51\x63\x55\xA5\x01\x10\xA9\xCE\x91'
+        testRC5 = RC5.RC5(32, 12, key)
+        plainText = b'\x21\xA5\xDB\xEE\x15\x4B\x8F\x6D'
+        with open(self.input_filename, 'wb') as data_file:
+            data_file.write(plainText)
+        testRC5.encryptFile(self.input_filename, self.out_filename)
+        cipherText = b'\xF7\xC0\x13\xAC\x5B\x2B\x89\x52'
+        with open(self.out_filename, 'rb') as data_file:
+            assert cipherText == data_file.read()
+        testRC5.decryptFile(self.out_filename, self.input_filename)
+        with open(self.input_filename, 'rb') as data_file:
+            assert plainText == data_file.read()
 
-    def test_RC5_eighteen_rounds_crypt(self):
-        testRC5 = RC5.RC5(64, 18, TEST_KEY)
-        testRC5.encrypt(self.input_filename, self.out_filename)
-        testRC5.decrypt(self.out_filename, self.input_filename)
-        with open(self.input_filename, 'r') as f:
-            assert INIT_DATA == f.read()
+    def test3(self):
+        key = b'\x78\x33\x48\xE7\x5A\xEB\x0F\x2F\xD7\xB1\x69\xBB\x8D\xC1\x67\x87'
+        testRC5 = RC5.RC5(32, 12, key)
+        plainText = b'\xF7\xC0\x13\xAC\x5B\x2B\x89\x52'
+        with open(self.input_filename, 'wb') as data_file:
+            data_file.write(plainText)
+        testRC5.encryptFile(self.input_filename, self.out_filename)
+        cipherText = b'\x2F\x42\xB3\xB7\x03\x69\xFC\x92'
+        with open(self.out_filename, 'rb') as data_file:
+            assert cipherText == data_file.read()
+        testRC5.decryptFile(self.out_filename, self.input_filename)
+        with open(self.input_filename, 'rb') as data_file:
+            assert plainText == data_file.read()
 
-    def test_RC5_twentyfour_rounds_crypt(self):
-        testRC5 = RC5.RC5(64, 24, TEST_KEY)
-        testRC5.encrypt(self.input_filename, self.out_filename)
-        testRC5.decrypt(self.out_filename, self.input_filename)
-        with open(self.input_filename, 'r') as f:
-            assert INIT_DATA == f.read()
+    def test4(self):
+        key = b'\xDC\x49\xDB\x13\x75\xA5\x58\x4F\x64\x85\xB4\x13\xB5\xF1\x2B\xAF'
+        testRC5 = RC5.RC5(32, 12, key)
+        plainText = b'\x2F\x42\xB3\xB7\x03\x69\xFC\x92'
+        with open(self.input_filename, 'wb') as data_file:
+            data_file.write(plainText)
+        testRC5.encryptFile(self.input_filename, self.out_filename)
+        cipherText = b'\x65\xC1\x78\xB2\x84\xD1\x97\xCC'
+        with open(self.out_filename, 'rb') as data_file:
+            assert cipherText == data_file.read()
+        testRC5.decryptFile(self.out_filename, self.input_filename)
+        with open(self.input_filename, 'rb') as data_file:
+            assert plainText == data_file.read()
 
-    def test_RC5_thirty_rounds_crypt(self):
-        testRC5 = RC5.RC5(64, 30, TEST_KEY)
-        testRC5.encrypt(self.input_filename, self.out_filename)
-        testRC5.decrypt(self.out_filename, self.input_filename)
-        with open(self.input_filename, 'r') as f:
-            assert INIT_DATA == f.read()
+    def test5(self):
+        key = b'\x52\x69\xF1\x49\xD4\x1B\xA0\x15\x24\x97\x57\x4D\x7F\x15\x31\x25'
+        testRC5 = RC5.RC5(32, 12, key)
+        plainText = b'\x65\xC1\x78\xB2\x84\xD1\x97\xCC'
+        with open(self.input_filename, 'wb') as data_file:
+            data_file.write(plainText)
+        testRC5.encryptFile(self.input_filename, self.out_filename)
+        cipherText = b'\xEB\x44\xE4\x15\xDA\x31\x98\x24'
+        with open(self.out_filename, 'rb') as data_file:
+            assert cipherText == data_file.read()
+        testRC5.decryptFile(self.out_filename, self.input_filename)
+        with open(self.input_filename, 'rb') as data_file:
+            assert plainText == data_file.read()
 
-    def test_RC5_thirtysix_rounds_crypt(self):
-        testRC5 = RC5.RC5(64, 6, TEST_KEY)
-        testRC5.encrypt(self.input_filename, self.out_filename)
-        testRC5.decrypt(self.out_filename, self.input_filename)
-        with open(self.input_filename, 'r') as f:
-            assert INIT_DATA == f.read()
 
+class RC5BytesTests(unittest.TestCase):
+
+    def test1(self):
+        testRC5 = RC5.RC5(32, 12, b'\0' * 16)
+        plainText = b'\0' * 8
+        cipherText = b'\x21\xA5\xDB\xEE\x15\x4B\x8F\x6D'
+        assert testRC5.encryptBytes(plainText) == cipherText
+
+    def test2(self):
+        key = b'\x91\x5F\x46\x19\xBE\x41\xB2\x51\x63\x55\xA5\x01\x10\xA9\xCE\x91'
+        testRC5 = RC5.RC5(32, 12, key)
+        plainText = b'\x21\xA5\xDB\xEE\x15\x4B\x8F\x6D'
+        cipherText = b'\xF7\xC0\x13\xAC\x5B\x2B\x89\x52'
+        assert testRC5.encryptBytes(plainText) == cipherText
+
+    def test3(self):
+        key = b'\x78\x33\x48\xE7\x5A\xEB\x0F\x2F\xD7\xB1\x69\xBB\x8D\xC1\x67\x87'
+        testRC5 = RC5.RC5(32, 12, key)
+        plainText = b'\xF7\xC0\x13\xAC\x5B\x2B\x89\x52'
+        cipherText = b'\x2F\x42\xB3\xB7\x03\x69\xFC\x92'
+        assert testRC5.encryptBytes(plainText) == cipherText
+
+    def test4(self):
+        key = b'\xDC\x49\xDB\x13\x75\xA5\x58\x4F\x64\x85\xB4\x13\xB5\xF1\x2B\xAF'
+        testRC5 = RC5.RC5(32, 12, key)
+        plainText = b'\x2F\x42\xB3\xB7\x03\x69\xFC\x92'
+        cipherText = b'\x65\xC1\x78\xB2\x84\xD1\x97\xCC'
+        assert testRC5.encryptBytes(plainText) == cipherText
+
+    def test5(self):
+        key = b'\x52\x69\xF1\x49\xD4\x1B\xA0\x15\x24\x97\x57\x4D\x7F\x15\x31\x25'
+        testRC5 = RC5.RC5(32, 12, key)
+        plainText = b'\x65\xC1\x78\xB2\x84\xD1\x97\xCC'
+        cipherText = b'\xEB\x44\xE4\x15\xDA\x31\x98\x24'
+        assert testRC5.encryptBytes(plainText) == cipherText
 
 if __name__ == "__main__":
     unittest.main()
